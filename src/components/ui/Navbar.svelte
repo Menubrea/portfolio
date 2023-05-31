306c31c117a06ddf5646e47ccb2899b14919c864
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-05-31 19:25:05 +0200

    Added navbar

---
+++ src/components/ui/Navbar.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { AppBar } from '@skeletonlabs/skeleton';
+	import { Button } from './';
+	import logo from '../../assets/logo-dark.svg';
+	import mobileLogo from '../../assets/short-logo.svg';
+</script>
+
+<AppBar>
+	<svelte:fragment slot="lead">
+		<img class="w-20 sm:hidden" src={mobileLogo} alt="website logo" />
+		<img class="w-48 hidden sm:block" src={logo} alt="website logo" />
+	</svelte:fragment>
+	<svelte:fragment slot="trail">
+		<Button>Github</Button>
+	</svelte:fragment>
+</AppBar>
