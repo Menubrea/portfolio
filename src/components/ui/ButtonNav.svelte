56d580578d9f30a48e94e9c263dd445b76075e5c
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-06-06 14:27:54 +0200

    ButtonNav component

---
+++ src/components/ui/ButtonNav.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { Button } from './';
+</script>
+
+<nav class="mt-5">
+	<a tabindex="-1" href="/about"><Button>About</Button></a>
+	<a tabindex="-1" href="/projects"><Button>Projects</Button></a>
+	<a tabindex="-1" href="/contact"><Button>Contact</Button></a>
+</nav>
