37533811d73c6de82789ff7beecc017bffd98ac5
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-06-06 14:29:39 +0200

    barrel export files

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,5 @@
 // Barrel exports for UI components
 export { default as Navbar } from './Navbar.svelte';
 export { default as Button } from './Button.svelte';
+export { default as Anchor } from './Anchor.svelte';
+export { default as ButtonNav } from './ButtonNav.svelte';
