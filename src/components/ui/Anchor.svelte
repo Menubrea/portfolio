bb049ded8bb6b6e3fdd18893218fb9fb0cd1d753
Truls Haakenstad <truls.haakenstad@outlook.com>
2023-06-06 14:28:46 +0200

    Anchor component

---
+++ src/components/ui/Anchor.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let href: string;
+</script>
+
+<a {href} class="variant-filled-secondary rounded-sm px-2 py-1 text-sm"
+	><span aria-hidden class="mr-1">[</span><slot /><span aria-hidden class="ml-1">]</span></a
+>
+
+<style>
+	a:hover {
+		outline: 1px dashed black;
+	}
+</style>
